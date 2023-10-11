91f418fd60409659f7d6fe3b34d3c2a9935e44ad
Ankur Kumar <98178905+Ankur-Kumar-4@users.noreply.github.com>
2023-10-11 13:20:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,20 +18,16 @@ class Person {
 
 class Student extends Person {
 	 study() {
-    console.log(`${this._name} is studying.`);
+    console.log(`${this._name} is studying`);
   }
 }
 
 class Teacher extends Person {
 	 teach() {
-    console.log(`${this._name} is teaching.`);
+    console.log(`${this._name} is teaching`);
   }
 }
-const student1 = new Student("Alice", 20);
-student1.study(); // Output: Alice is studying.
 
-const teacher1 = new Teacher("Bob", 35);
-teacher1.teach(); // Output: Bob is teaching.
 
 // Do not change the code below this line
 window.Person = Person;
